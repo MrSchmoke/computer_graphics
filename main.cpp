328b3462262853d70e3c6b48b4a99f27b014372a
MrSchmoke <martin.staats@gmail.com>
2022-08-03 14:07:05 +0200

    windows compability added

--- main.cpp
+++ main.cpp
@@ -33,7 +33,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);libglfw3.3.dylib
 #endif
 
     // glfw window creation
