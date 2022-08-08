d92ef34be35e9c2edb3cff6b0201e4f905f0bb38
MrSchmoke <martin.staats@gmail.com>
2022-08-08 23:57:04 +0200

    further reorganisation

---
+++ opengl/src/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+
+#include <glad.h>
+
+#define GLFW_INCLUDE_NONE //explicitly disable inclusion of environment header
+#include <GLFW/glfw3.h>
+
+int main(void)
+{
+    return 0;
+}
